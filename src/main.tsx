ccee9bc0bbb31012efd57ca12ac3e19b09bf7324
Ayman Amin Ali Amin <ayman1793@hotmail.com>
2026-08-21 02:03:22 +0300

    style(app): load modal primitives

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { App } from './app/App'
 import { I18nProvider } from './lib/i18n'
 import './app/styles.css'
 import './app/refinements.css'
+import './app/modals.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode><I18nProvider><App /></I18nProvider></StrictMode>,
