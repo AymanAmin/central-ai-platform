a06928850b88a4e03805b4305068fc447c7c7ab5
Ayman Amin Ali Amin <ayman1793@hotmail.com>
2026-08-21 16:56:21 +0300

    Load organization agents responsive styles

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,7 @@ import './app/modals.css'
 import './app/admin-actions.css'
 import './app/web-chat.css'
 import './app/customer-chat-continuity.css'
+import './app/agent-plans.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode><I18nProvider><App /></I18nProvider></StrictMode>,
